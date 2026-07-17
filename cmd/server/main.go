ecf06f586ba82f7e646cc827b6534b08ea35f84e
zadescoxp <zadescoxp@gmail.com>
2026-07-17 14:40:02 +0530

    connected with routes and config

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"github.com/zadescoxp/GoCord/internal/config"
+	"github.com/zadescoxp/GoCord/internal/routes"
+)
+
+func main() {
+	cfg := config.LoadConfig()
+	router := routes.SetupRouter()
+
+	log.Printf("🚀 Server running on port %s\n", cfg.Port)
+
+	err := router.Run(fmt.Sprintf(":%s", cfg.Port))
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
