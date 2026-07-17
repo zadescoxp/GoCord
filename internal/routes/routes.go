cf95bf358c37ad02a6eaf5db0168c775a95c49d1
zadescoxp <zadescoxp@gmail.com>
2026-07-17 17:08:10 +0530

    added a new demo route with routes nesting

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -5,17 +5,32 @@ import "github.com/gin-gonic/gin"
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Welcome to GoCord🔥",
-		})
-	})
+	api := router.Group("/api")
+	{
+		v1 := api.Group("/v1")
+		{
 
-	router.GET("/health", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "System is healthy",
-		})
-	})
+			v1.GET("/", func(c *gin.Context) {
+				c.JSON(200, gin.H{
+					"message": "Welcome to GoCord🔥",
+				})
+			})
+
+			v1.GET("/health", func(c *gin.Context) {
+				c.JSON(200, gin.H{
+					"message": "System is healthy",
+				})
+			})
+
+			v1.GET("/info", func(c *gin.Context) {
+				c.JSON(200, gin.H{
+					"name":    "GoCord",
+					"version": "1.0.0",
+					"author":  "Your Name",
+				})
+			})
+		}
+	}
 
 	return router
 }
